eefa2f9f6d173ccf7545efeadbac72ac00ae4159
EdumanReyes <104949585+EdumanReyes@users.noreply.github.com>
2023-06-12 12:23:50 -0600

    Add files via upload

---
+++ LeerXML/LeerXML/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+
+namespace LeerXML { 
+    class Program
+    {
+        static void Main(string[] args) {
+            try { 
+            XmlDocument confi = new XmlDocument();
+            confi.Load("Configuraciones.xml");
+
+            foreach (XmlNode n1 in confi.DocumentElement.ChildNodes) //Recorrer nodo raiz n1
+            {   
+                string name = getValorAtributo(n1, "name"); // Función para obtener el valor del atributo deseado
+                Console.WriteLine(name);
+                    if (n1.HasChildNodes)//Revisamos si el nodo raiz tiene hijos
+                    {
+                        foreach (XmlNode n2 in n1.ChildNodes)   //Como los hijos del nodo 1 tienen mas hijos tambien los recorremos 
+                        {
+                            Console.WriteLine($"{n2.Name} {n2.InnerText}"); //Se acceden a los valores de los atributos del nodo2  
+                        }
+                    }
+                    else {
+                        Console.WriteLine("No existen más atributos");
+                    }
+                Console.WriteLine();
+            }
+            
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Error:"+ex.Message);
+            }
+
+        }
+            
+        static string getValorAtributo(XmlNode node, string nombreAtributo)
+        {
+            XmlAttribute atributo = node.Attributes[nombreAtributo];
+            return atributo != null ? atributo.Value : null; //Si el atributo no existe devuelve null
+
+        }
+    }
+}
